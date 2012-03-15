22473fde91ae5c9861e9d22c0c1aeec0e2a93489
Ma Xiaojun <damage3025@gmail.com>
2012-03-15 12:56:58 +0800

    first commit

---
+++ SocketWrapper.h
@@ -0,0 +1,69 @@
+/* Handle Header Files */
+#if defined _WIN32
+	#pragma comment(lib, "ws2_32.lib")
+	#include <winsock2.h>
+	#include <ws2tcpip.h>
+#else
+	#include <sys/types.h>
+	#include <sys/socket.h>
+	#include <netinet/in.h>
+	#include <arpa/inet.h>
+	#include <netdb.h>
+	#include <unistd.h>
+	#include <errno.h>
+#endif
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Handle Minor Differences */
+#if defined _WIN32
+	#define SOCKERR() WSAGetLastError() 
+#else 
+	typedef int SOCKET; 
+	#define INVALID_SOCKET -1 
+	#define SOCKET_ERROR -1
+	#define closesocket(s) close(s) 
+	#define SOCKERR() errno 
+#endif 
+
+/* Define init_sockets() Macro */
+#if defined _WIN32 
+#define init_sockets()\
+do {\
+	printf("Initializing winsock environment...");\
+	WSADATA wsaData;\
+	if (WSAStartup(MAKEWORD(2,2), &wsaData) != NO_ERROR){\
+		printf("Error at WSAStartup()\nRobot exit.");\
+		exit(1);\
+	}\
+	printf("Done\n");\
+} while(0)
+#else
+#define init_sockets() (void)0
+#endif 
+
+/* Define uninit_sockets() Macro */
+#if defined _WIN32
+#define uninit_sockets()\
+do {\
+	WSACleanup();\
+} while(0)
+#else
+#define uninit_sockets() (void)0
+#endif
+
+
+/* Define SLEEP() Macro */
+#if defined _WIN32
+#define SLEEP(ms) Sleep(ms)
+#else
+#define SLEEP(ms)\
+do {\
+	unsigned n = (ms);\
+	timeval timeout;\
+	timeout.tv_sec = n / 1000;\
+	timeout.tv_usec = (n % 1000) * 1000;\
+	select(0 ,NULL, NULL, NULL, &timeout);\
+} while(0)
+#endif
+
